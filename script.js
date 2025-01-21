c68b379a4f2199d4b7e370500ffa2c5ab881b5e3
Richard-Raph <richardakpan77@gmail.com>
2025-01-21 12:07:51 +0100

    `Added active class to card-img element and updated script.js to handle card image clicks`

---
+++ script.js
@@ -0,0 +1,9 @@
+let imgs = document.querySelectorAll('.card-img img');
+let cardImgs = document.querySelectorAll('.card-img');
+
+imgs.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        cardImgs.forEach(card => card.classList.remove('active'));
+        cardImgs[index].classList.add('active');
+    });
+});
